330616adace3103adbe7f1df634b37d2945dc50e
Mircea M <mircea.moise@gmail.com>
2021-01-31 18:15:15 +0000

    Simple passthrough

---
+++ src/main/java/uk/nhs/prm/deductions/gp2gpmessagehandler/JmsConsumer.java
@@ -0,0 +1,33 @@
+package uk.nhs.prm.deductions.gp2gpmessagehandler;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.jms.TextMessage;
+
+@Component
+public class JmsConsumer {
+
+    final
+    JmsTemplate jmsTemplate;
+
+    @Value("${activemq.outboundQueue}")
+    private String outboundQueue;
+
+    public JmsConsumer(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    //might need to use BytesMessage as that's how is probably gonna be read from the q
+    @JmsListener(destination = "${activemq.inboundQueue}")
+    public void onMessage(TextMessage message) {
+        try {
+            System.out.println("Received Message: "+ message.getText());
+            jmsTemplate.convertAndSend(outboundQueue, message.getText());
+        } catch(Exception e) {
+            System.out.println("Received Exception : "+ e);
+        }
+    }
+}
--- src/test/java/uk/nhs/prm/deductions/gp2gpmessagehandler/Gp2gpMessageHandlerApplicationTests.java
+++ src/test/java/uk/nhs/prm/deductions/gp2gpmessagehandler/Gp2gpMessageHandlerApplicationTests.java
@@ -6,6 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jms.core.JmsTemplate;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class Gp2gpMessageHandlerApplicationTests {
 	@Autowired
@@ -14,12 +22,25 @@ class Gp2gpMessageHandlerApplicationTests {
 	@Value("${activemq.inboundQueue}")
 	private String inboundQueue;
 
+	@Value("${activemq.outboundQueue}")
+	private String outboundQueue;
+
 	@Test
-	void contextLoads() {
+	void shouldPassThroughMessages() {
 		//action: send a message on the inbound q
-		jmsTemplate.convertAndSend(inboundQueue,"test message");
+		String testMessage = "test message";
+		jmsTemplate.convertAndSend(inboundQueue, testMessage);
 
 		//assertion: verify the message gets on the outbound q
+		jmsTemplate.setReceiveTimeout(5000);
+		Message message = jmsTemplate.receive(outboundQueue);
+		assertNotNull(message);
+		TextMessage textMessage = (TextMessage) message;
+		try {
+			assertThat(textMessage.getText(), equalTo(testMessage));
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
