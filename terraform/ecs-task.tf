f89b26d3be74018d5f38697143c1bb40ae4906e3
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-02-10 10:41:55 +0000

    [PRMT-1526] Renamed env variables to gp2gp_message_handler and added default values for queue urls

--- terraform/ecs-task.tf
+++ terraform/ecs-task.tf
@@ -4,13 +4,13 @@ locals {
     task_ecr_url                 = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
     task_log_group               = "/nhs/deductions/${var.environment}-${data.aws_caller_identity.current.account_id}/${var.component_name}"
     environment_variables        = [
-      { name = "GP2GP_WORKER_MHS_QUEUE_NAME", value = "js-inbound" },
-        { name = "GP2GP_WORKER_MHS_QUEUE_URL_1", value = data.aws_ssm_parameter.openwire_endpoint_0.value },
-        { name = "GP2GP_WORKER_MHS_QUEUE_URL_2", value = data.aws_ssm_parameter.openwire_endpoint_1.value },
+      { name = "GP2GP_MESSAGE_HANDLER_MHS_QUEUE_NAME", value = "js-inbound" },
+      { name = "GP2GP_MESSAGE_HANDLER_MHS_QUEUE_URL_1", value = data.aws_ssm_parameter.openwire_endpoint_0.value },
+      { name = "GP2GP_MESSAGE_HANDLER_MHS_QUEUE_URL_2", value = data.aws_ssm_parameter.openwire_endpoint_1.value },
     ]
     secret_environment_variables = [
-      { name = "GP2GP_WORKER_MHS_QUEUE_USERNAME", valueFrom = data.aws_ssm_parameter.amq-username.arn },
-      { name = "GP2GP_WORKER_MHS_QUEUE_PASSWORD", valueFrom = data.aws_ssm_parameter.amq-password.arn },
+      { name = "GP2GP_MESSAGE_HANDLER_MHS_QUEUE_USERNAME", valueFrom = data.aws_ssm_parameter.amq-username.arn },
+      { name = "GP2GP_MESSAGE_HANDLER_MHS_QUEUE_PASSWORD", valueFrom = data.aws_ssm_parameter.amq-password.arn },
     ]
 }
 
