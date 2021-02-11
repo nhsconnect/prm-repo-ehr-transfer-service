e70011e3e6c34737233ad2f46b838e65c0459ac0
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-02-11 09:30:08 +0000

    [PRMT-1526] Refactored integration tests to readBytes instead of readUtf and throw exception

--- src/test/java/uk/nhs/prm/deductions/gp2gpmessagehandler/integrationTests/Gp2gpMessageHandlerApplicationTests.java
+++ src/test/java/uk/nhs/prm/deductions/gp2gpmessagehandler/integrationTests/Gp2gpMessageHandlerApplicationTests.java
@@ -35,53 +35,50 @@ class Gp2gpMessageHandlerApplicationTests {
 	@Value("${activemq.unhandledQueue}")
 	private String unhandledQueue;
 
-	private TestDataLoader dataLoader = new TestDataLoader();
+    private TestDataLoader dataLoader = new TestDataLoader();
 
-	@Test
-	void shouldPassThroughMessagesForOldWorker() throws IOException {
-		String ehrRequest = dataLoader.getData("ehrRequestRCMR_IN010000UK05InteractionId.xml");
-		jmsTemplate.send(inboundQueue, new MessageCreator() {
-			@Override
-			public Message createMessage(Session session) throws JMSException {
-				BytesMessage bytesMessage = session.createBytesMessage();
-				bytesMessage.writeBytes(ehrRequest.getBytes(StandardCharsets.UTF_8));
-				return bytesMessage;
-			}
-		});
+    @Test
+    void shouldPassThroughMessagesForOldWorker() throws IOException, JMSException {
+        String ehrRequest = dataLoader.getData("ehrRequestRCMR_IN010000UK05InteractionId.xml");
+        jmsTemplate.send(inboundQueue, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                BytesMessage bytesMessage = session.createBytesMessage();
+                bytesMessage.writeBytes(ehrRequest.getBytes(StandardCharsets.UTF_8));
+                return bytesMessage;
+            }
+        });
 
-		jmsTemplate.setReceiveTimeout(5000);
-		BytesMessage message = (BytesMessage) jmsTemplate.receive(outboundQueue);
-		assertNotNull(message);
-		try {
-			String stringMessage = message.readUTF();
-			assertThat(stringMessage, equalTo(ehrRequest));
-		} catch (JMSException e) {
-			e.printStackTrace();
-		}
-	}
+        jmsTemplate.setReceiveTimeout(5000);
+        BytesMessage message = (BytesMessage) jmsTemplate.receive(outboundQueue);
+        assertNotNull(message);
 
-	@Test
-	void shouldSendMalformedMessagesToUnhandledQ() {
-		String malformedMessage = "clearly not a GP2GP message";
+		byte[] allTheBytes = new byte[(int) message.getBodyLength()];
+		message.readBytes(allTheBytes);
+		String messageAsString = new String(allTheBytes, StandardCharsets.UTF_8);
+        assertThat(messageAsString, equalTo(ehrRequest));
+    }
 
-		jmsTemplate.send(inboundQueue, new MessageCreator() {
-			@Override
-			public Message createMessage(Session session) throws JMSException {
-				BytesMessage bytesMessage = session.createBytesMessage();
-				bytesMessage.writeUTF(malformedMessage);
-				return bytesMessage;
-			}
-		});
+    @Test
+    void shouldSendMalformedMessagesToUnhandledQ() throws JMSException {
+        String malformedMessage = "clearly not a GP2GP message";
+
+        jmsTemplate.send(inboundQueue, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                BytesMessage bytesMessage = session.createBytesMessage();
+                bytesMessage.writeBytes(malformedMessage.getBytes(StandardCharsets.UTF_8));
+                return bytesMessage;
+            }
+        });
 
 		jmsTemplate.setReceiveTimeout(5000);
 		BytesMessage message = (BytesMessage) jmsTemplate.receive(unhandledQueue);
 		assertNotNull(message);
-		try {
-			String stringMessage = message.readUTF();
-			assertThat(stringMessage, equalTo(malformedMessage));
 
-		} catch (JMSException e) {
-			e.printStackTrace();
-		}
+        byte[] allTheBytes = new byte[(int) message.getBodyLength()];
+        message.readBytes(allTheBytes);
+        String messageAsString = new String(allTheBytes, StandardCharsets.UTF_8);
+        assertThat(messageAsString, equalTo(malformedMessage));
 	}
 }
