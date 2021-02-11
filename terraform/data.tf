2fafafba45924fb3c751633478edf34224f15e5d
ewrobel-nhs <edyta.wrobel@nhs.net>
2021-02-11 12:34:48 +0000

    [PRMT-1526] Renamed the message handler sg id to correct syntax

--- terraform/data.tf
+++ terraform/data.tf
@@ -12,8 +12,8 @@ data "aws_ssm_parameter" "deductions_private_vpc_id" {
   name = "/repo/${var.environment}/output/prm-deductions-infra/private-vpc-id"
 }
 
-data "aws_ssm_parameter" "deductions_private_gp2gp-message_handler_sg_id" {
-  name = "/repo/${var.environment}/output/prm-deductions-infra/deductions-private-gp2gp-message_handler-sg-id"
+data "aws_ssm_parameter" "deductions_private_gp2gp_message_handler_sg_id" {
+  name = "/repo/${var.environment}/output/prm-deductions-infra/deductions-private-gp2gp-message-handler-sg-id"
 }
 
 data "aws_ssm_parameter" "amq-username" {
