8e873b4ce25a9e9ffb15c6dbbbc5853760edaf4a
Mircea M <mircea.moise@gmail.com>
2021-03-18 18:38:20 +0000

    [PRMT-1278] Suppressed spotbugs as it's a false positive

--- src/main/java/uk/nhs/prm/deductions/gp2gpmessagehandler/jsonModels/EhrExtractMessage.java
+++ src/main/java/uk/nhs/prm/deductions/gp2gpmessagehandler/jsonModels/EhrExtractMessage.java
@@ -1,9 +1,11 @@
 package uk.nhs.prm.deductions.gp2gpmessagehandler.jsonModels;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.UUID;
 
 public class EhrExtractMessage {
 
+    @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
     public UUID ehrExtractMessageId;
 
     public EhrExtractMessage(UUID ehrExtractMessageId) {
