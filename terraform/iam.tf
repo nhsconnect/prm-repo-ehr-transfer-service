53dcc0cd4fba3e266ce952b39161e834cb4e6c3d
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-03-19 13:32:54 +0000

    [PRMT-1278] Added iam permissions to access ssm parameters for gp to repo

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -68,6 +68,8 @@ data "aws_iam_policy_document" "ssm_policy_doc" {
     resources = [
       "arn:aws:ssm:${var.region}:${local.account_id}:parameter/repo/${var.environment}/user-input/mq-app-username",
       "arn:aws:ssm:${var.region}:${local.account_id}:parameter/repo/${var.environment}/user-input/mq-app-password",
+      "arn:aws:ssm:${var.region}:${local.account_id}:parameter/repo/${var.environment}/user-input/gp-to-repo-authorization-keys",
+      "arn:aws:ssm:${var.region}:${local.account_id}:parameter/repo/${var.environment}/output/prm-deductions-gp-to-repo/service-url",
     ]
   }
 }
