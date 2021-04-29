467b72a0d8c153901bd20b97df9508f60410a1a8
aiwatko <oktawia.kata1@nhs.net>
2021-04-29 14:43:12 +0100

    [PRMT-1990] Refactored ehr-repo URL to use new pattern

--- terraform/ecs-task.tf
+++ terraform/ecs-task.tf
@@ -8,7 +8,7 @@ locals {
       { name = "GP2GP_MESSAGE_HANDLER_MHS_QUEUE_URL_2", value = data.aws_ssm_parameter.openwire_endpoint_1.value },
       { name = "GP2GP_MESSAGE_HANDLER_GP_TO_REPO_URL", value = "https://gp-to-repo.${var.environment}.non-prod.patient-deductions.nhs.uk" },
       { name = "GP2GP_MESSAGE_HANDLER_REPO_TO_GP_URL", value = "https://repo-to-gp.${var.environment}.non-prod.patient-deductions.nhs.uk" },
-      { name = "GP2GP_MESSAGE_HANDLER_EHR_REPO_URL", value = "https://${var.environment}.ehr-repo.patient-deductions.nhs.uk" },
+      { name = "GP2GP_MESSAGE_HANDLER_EHR_REPO_URL", value = "https://ehr-repo.${var.environment}.non-prod.patient-deductions.nhs.uk" },
     ]
     secret_environment_variables = [
       { name = "GP2GP_MESSAGE_HANDLER_MHS_QUEUE_USERNAME", valueFrom = data.aws_ssm_parameter.amq-username.arn },
