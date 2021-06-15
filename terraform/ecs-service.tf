54f5dec61d84b37b56154d3cf686b8a17390c446
Oktawia Kata <oktawia.kata1@nhs.net>
2021-06-15 16:13:53 +0100

    [PRMT-2024] Added ingress rule for gp2gp-message-handler to reach ehr-repo

--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -16,3 +16,17 @@ resource "aws_ecs_service" "ecs-service" {
     subnets         = local.private_subnets
   }
 }
+
+data "aws_ssm_parameter" "service-to-ehr-repo-sg-id" {
+  name = "/repo/${var.environment}/output/prm-deductions-ehr-repository/service-to-ehr-repo-sg-id"
+}
+
+resource "aws_security_group_rule" "gp2gp-message-handler-to-ehr-repo" {
+  type = "ingress"
+  protocol = "TCP"
+  from_port = 443
+  to_port = 443
+  security_group_id = data.aws_ssm_parameter.service-to-ehr-repo-sg-id.value
+  source_security_group_id = data.aws_ssm_parameter.deductions_private_gp2gp_message_handler_sg_id.value
+}
+
