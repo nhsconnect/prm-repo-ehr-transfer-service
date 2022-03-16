cdb4d9cd7e4673f659a0c8f0fd2114ff76ee82d2
Rosie Amphlett <rosie.amphlett1@nhs.net>
2022-03-16 10:22:51 +0000

    [PRMT-2556] Updated dependencies and removed test coverage violation rules

--- src/main/java/uk/nhs/prm/deductions/gp2gpmessagehandler/gp2gpMessageModels/Identifier.java
+++ src/main/java/uk/nhs/prm/deductions/gp2gpmessagehandler/gp2gpMessageModels/Identifier.java
@@ -1,9 +1,7 @@
 package uk.nhs.prm.deductions.gp2gpmessagehandler.gp2gpMessageModels;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import javax.xml.bind.annotation.XmlAttribute;
-import java.util.UUID;
+import jakarta.xml.bind.annotation.XmlAttribute;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Identifier {
