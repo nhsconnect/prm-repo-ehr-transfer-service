177073e55fb39931208e3930d09cb952bf5f13b7
syedsadiqali <syed.ali69@nhs.net>
2022-03-25 16:56:07 +0000

    [PRMT-2556]-Added logs for recording events in db

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/database/TransferTrackerService.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/database/TransferTrackerService.java
@@ -1,6 +1,7 @@
 package uk.nhs.prm.repo.ehrtransferservice.database;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.nhs.prm.repo.ehrtransferservice.ehrrequesthandler.ConversationIdStore;
 import uk.nhs.prm.repo.ehrtransferservice.ehrrequesthandler.RepoIncomingEvent;
@@ -11,6 +12,7 @@ import java.time.ZonedDateTime;
 
 @AllArgsConstructor
 @Service
+@Slf4j
 public class TransferTrackerService {
 
     private final TransferTrackerDb transferTrackerDb;
@@ -18,9 +20,15 @@ public class TransferTrackerService {
 
     public void recordEventInDb(RepoIncomingEvent incomingEvent) {
         String conversationId = conversationIdStore.getConversationId();
-        TransferTrackerDbEntry transferTrackerDbEntry =
-                new TransferTrackerDbEntry(conversationId, incomingEvent.getNhsNumber(), incomingEvent.getSourceGP(), incomingEvent.getNemsMessageId(), "ACTION:TRANSFER_TO_REPO_STARTED", getTimeNow());
-        transferTrackerDb.save(transferTrackerDbEntry);
+        try {
+            log.info("Recording an event in transfer tracker db with status : " + "ACTION:TRANSFER_TO_REPO_STARTED");
+            TransferTrackerDbEntry transferTrackerDbEntry =
+                    new TransferTrackerDbEntry(conversationId, incomingEvent.getNhsNumber(), incomingEvent.getSourceGP(), incomingEvent.getNemsMessageId(), "ACTION:TRANSFER_TO_REPO_STARTED", getTimeNow());
+            transferTrackerDb.save(transferTrackerDbEntry);
+        }catch(Exception e) {
+            log.error("Error encountered while recording event in transfer tracker db" + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
     private String getTimeNow() {
         return ZonedDateTime.now(ZoneOffset.ofHours(0)).toString();
