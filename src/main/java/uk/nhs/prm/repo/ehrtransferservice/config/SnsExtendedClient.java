ca36e78b7726b3c27e5b3a1309323cdb0402094a
Rosie Amphlett <rosie.amphlett1@nhs.net>
2022-04-19 09:43:07 +0100

    [PRMT-2558] Renamed amazon sns extended client bean to not have same name as the class

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/config/SnsExtendedClient.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/config/SnsExtendedClient.java
@@ -1,7 +1,6 @@
 package uk.nhs.prm.repo.ehrtransferservice.config;
 
 import com.amazonaws.services.sns.AmazonSNS;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,13 +12,12 @@ import software.amazon.sns.AmazonSNSExtendedClient;
 import software.amazon.sns.SNSExtendedClientConfiguration;
 
 @Configuration
-@RequiredArgsConstructor
 public class SnsExtendedClient {
     @Value("${aws.sqsLargeMessageBucketName}")
     private String bucketName;
 
     @Bean
-    public AmazonSNSExtendedClient snsExtendedClient(AmazonSNS snsClient, S3Client s3) {
+    public AmazonSNSExtendedClient s3SupportedSnsClient(AmazonSNS snsClient, S3Client s3) {
         var s3BackedPayloadStore = new S3BackedPayloadStore(new S3Dao(s3), bucketName);
         PayloadStorageConfiguration payloadStorageConfiguration = new SNSExtendedClientConfiguration().withPayloadSupportEnabled(s3, bucketName);
         return new AmazonSNSExtendedClient(snsClient, (SNSExtendedClientConfiguration) payloadStorageConfiguration, s3BackedPayloadStore);
