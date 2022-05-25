0f63c8ed91e39d392d1627c16c4aced57790c9e9
romanenhs <romane.castera1@nhs.net>
2022-05-25 16:33:26 +0100

    Revert "[PRMT-2637] Removed flaky repo-incoming integration assertion"
    This reverts commit d3cb16f75f9fc66e9476bd574a8e385e284dd812.

--- src/integration/java/uk/nhs/prm/repo/ehrtransferservice/repo_incoming/EhrRequestTest.java
+++ src/integration/java/uk/nhs/prm/repo/ehrtransferservice/repo_incoming/EhrRequestTest.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.sqs.model.PurgeQueueRequest;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -93,6 +94,9 @@ class EhrRequestTest {
             assertThat(dbClientItem.get("source_gp").s()).isEqualTo(SOURCE_GP);
             assertThat(dbClientItem.get("conversation_id").s()).isEqualTo(CONVERSATION_ID);
         });
+
+        verify(postRequestedFor(urlMatching("/health-record-requests/" + NHS_NUMBER)));
+
     }
 
     private String getRepoIncomingData() {
