865c89cf4573ef229f66c172614c39156647ebdf
syedsadiqali <syed.ali69@nhs.net>
2022-05-30 14:34:25 +0100

    [PRMT-2650] Added S3 pointer message

---
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/json_models/S3PointerMessage.java
@@ -0,0 +1,16 @@
+package uk.nhs.prm.repo.ehrtransferservice.json_models;
+
+
+import com.google.gson.JsonObject;
+import lombok.Getter;
+
+@Getter
+public class S3PointerMessage {
+    String s3BucketName;
+    String s3Key;
+
+    public S3PointerMessage(JsonObject s3PointerJsonObject) {
+        this.s3BucketName = s3PointerJsonObject.get("s3BucketName").getAsString();
+        this.s3Key = s3PointerJsonObject.get("s3Key").getAsString();
+    }
+}
