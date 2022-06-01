4d2755942719d88d396f2bd4f766194a94285734
Mario Giampietri <mario.giampietri1@nhs.net>
2022-06-01 18:35:47 +0100

    [PRMT-2650] This reverts commit 1f90d2ba0ec72db8d189f4e0396462bdcbfe8d66
    Still not happy with how Parser, ParsedMessage, and related classes are used
    and combined, I might try a better refactoring later.

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/gp2gp_message_models/ParsedMessage.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/gp2gp_message_models/ParsedMessage.java
@@ -1,15 +1,43 @@
 package uk.nhs.prm.repo.ehrtransferservice.gp2gp_message_models;
 
+import org.apache.activemq.command.ActiveMQBytesMessage;
+
+import javax.jms.BytesMessage;
+import javax.jms.JMSException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class ParsedMessage extends BaseParsedMessage {
+public class ParsedMessage {
+    private SOAPEnvelope soapEnvelope;
     private MessageContent messageContent;
+    private String rawMessage;
 
     public ParsedMessage(SOAPEnvelope soapEnvelope, MessageContent messageContent, String rawMessage) {
-        super(soapEnvelope, rawMessage);
+        this.soapEnvelope = soapEnvelope;
         this.messageContent = messageContent;
+        this.rawMessage = rawMessage;
+    }
+
+    public BytesMessage getBytesMessage() throws JMSException {
+        final byte[] bytesArray = this.rawMessage.getBytes(StandardCharsets.UTF_8);
+        ActiveMQBytesMessage bytesMessage = new ActiveMQBytesMessage();
+        bytesMessage.writeBytes(bytesArray);
+        bytesMessage.reset();
+        return bytesMessage;
+    }
+
+    public String getRawMessage() {
+        return rawMessage;
+    }
+
+    public MessageContent getMessageContent() {
+        return messageContent;
+    }
+
+    public SOAPEnvelope getSoapEnvelope() {
+        return soapEnvelope;
     }
 
     public String getNhsNumber() {
@@ -21,6 +49,30 @@ public class ParsedMessage extends BaseParsedMessage {
             return ((EhrRequestMessageWrapper) messageContent).getEhrRequest().getPatient().getNhsNumber();
         }
         return null;
+    }
+
+    public String getInteractionId() {
+        SOAPEnvelope soapEnvelope = getSoapEnvelope();
+        if (soapEnvelope.header == null || soapEnvelope.header.messageHeader == null) {
+            return null;
+        }
+        return soapEnvelope.header.messageHeader.action;
+    }
+
+    public UUID getConversationId() {
+        SOAPEnvelope soapEnvelope = getSoapEnvelope();
+        if (soapEnvelope.header == null || soapEnvelope.header.messageHeader == null) {
+            return null;
+        }
+        return soapEnvelope.header.messageHeader.conversationId;
+    }
+
+    public UUID getMessageId() {
+        SOAPEnvelope soapEnvelope = getSoapEnvelope();
+        if (soapEnvelope.header == null || soapEnvelope.header.messageHeader == null || soapEnvelope.header.messageHeader.messageData == null) {
+            return null;
+        }
+        return soapEnvelope.header.messageHeader.messageData.messageId;
     }
 
     public String getEhrRequestId() {
