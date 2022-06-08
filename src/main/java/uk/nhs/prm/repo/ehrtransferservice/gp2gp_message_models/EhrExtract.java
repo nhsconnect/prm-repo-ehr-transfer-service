42ca36885e100f9b229b5309a3735ed00dc7a84e
Mario Giampietri <mario.giampietri1@nhs.net>
2022-06-08 13:39:38 +0100

    [PRMT-2650] Extended EhrExtract class to be able to expose ods codes

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/gp2gp_message_models/EhrExtract.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/gp2gp_message_models/EhrExtract.java
@@ -8,13 +8,39 @@ public class EhrExtract {
     @JacksonXmlProperty(localName = "recordTarget")
     public RecordTarget recordTarget;
 
+    @JacksonXmlProperty(localName = "author")
+    public Author author;
+
+    @JacksonXmlProperty(localName = "destination")
+    public Destination destination;
+
     public Patient getPatient() {
         return this.recordTarget.patient;
+    }
+
+    public String getSourceGp() {
+        return this.author.requestingPractice.agentOrganizationSDS.id.extension;
+    }
+
+    public String getDestinationGp() {
+        return this.destination.requestingPractice.agentOrganizationSDS.id.extension;
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class RecordTarget {
         @JacksonXmlProperty(localName = "patient")
         public Patient patient;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Author {
+        @JacksonXmlProperty(localName = "AgentOrgSDS")
+        public RequestingPractice requestingPractice;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Destination {
+        @JacksonXmlProperty(localName = "AgentOrgSDS")
+        public RequestingPractice requestingPractice;
     }
 }
--- src/test/java/uk/nhs/prm/repo/ehrtransferservice/gp2gp_message_models/EhrExtractMessageWrapperTest.java
+++ src/test/java/uk/nhs/prm/repo/ehrtransferservice/gp2gp_message_models/EhrExtractMessageWrapperTest.java
@@ -10,27 +10,57 @@ import static org.hamcrest.Matchers.equalTo;
 
 @Tag("unit")
 public class EhrExtractMessageWrapperTest {
+    private final String extract = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<RCMR_IN030000UK06 xmlns=\"urn:hl7-org:v3\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"urn:hl7-org:v3 ..SchemasRCMR_IN030000UK06.xsd\">\n" +
+            "    <ControlActEvent classCode=\"CACT\" moodCode=\"EVN\">\n" +
+            "        <subject typeCode=\"SUBJ\" contextConductionInd=\"false\">\n" +
+            "            <EhrExtract classCode=\"EXTRACT\" moodCode=\"EVN\">\n" +
+            "                <recordTarget typeCode=\"RCT\">\n" +
+            "                    <patient classCode=\"PAT\">\n" +
+            "                        <id root=\"2.16.840.1.113883.2.1.4.1\" extension=\"9442964410\" />\n" +
+            "                    </patient>" +
+            "                </recordTarget>" +
+            "                <author typeCode=\"AUT\">" +
+            "                   <time value=\"20220608101110\"/>" +
+            "                   <signatureCode code=\"S\"/>" +
+            "                   <signatureText>X</signatureText>" +
+            "                   <AgentOrgSDS classCode=\"AGNT\">" +
+            "                       <agentOrganizationSDS classCode=\"ORG\" determinerCode=\"INSTANCE\">" +
+            "                          <id root=\"1.2.826.0.1285.0.1.10\" extension=\"N82668\"/>" +
+            "                       </agentOrganizationSDS>" +
+            "                   </AgentOrgSDS>" +
+            "               </author>" +
+            "              <destination typeCode=\"DST\">" +
+            "                   <AgentOrgSDS classCode=\"AGNT\">" +
+            "                       <agentOrganizationSDS classCode=\"ORG\" determinerCode=\"INSTANCE\">" +
+            "                           <id root=\"1.2.826.0.1285.0.1.10\" extension=\"B85002\"/>" +
+            "                       </agentOrganizationSDS>" +
+            "                   </AgentOrgSDS>" +
+            "               </destination>" +
+            "             </EhrExtract>" +
+            "         </subject>" +
+            "    </ControlActEvent>" +
+            "</RCMR_IN030000UK06>";
+
     @Test
     public void shouldRetrieveNHSNumberFromEHRExtract() throws JsonProcessingException {
-        String extract = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                "<RCMR_IN030000UK06 xmlns=\"urn:hl7-org:v3\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"urn:hl7-org:v3 ..SchemasRCMR_IN030000UK06.xsd\">\n" +
-                "    <ControlActEvent classCode=\"CACT\" moodCode=\"EVN\">\n" +
-                "        <subject typeCode=\"SUBJ\" contextConductionInd=\"false\">\n" +
-                "            <EhrExtract classCode=\"EXTRACT\" moodCode=\"EVN\">\n" +
-                "                <recordTarget typeCode=\"RCT\">\n" +
-                "                    <patient classCode=\"PAT\">\n" +
-                "                        <id root=\"2.16.840.1.113883.2.1.4.1\" extension=\"9442964410\" />\n" +
-                "                    </patient>" +
-                "                </recordTarget>" +
-                "             </EhrExtract>" +
-                "         </subject>" +
-                "    </ControlActEvent>" +
-                "</RCMR_IN030000UK06>";
-
-        XmlMapper xmlMapper = new XmlMapper();
-        EhrExtractMessageWrapper ehrExtractMessageWrapper = xmlMapper.readValue(extract, EhrExtractMessageWrapper.class);
-        EhrExtract ehrExtract = ehrExtractMessageWrapper.getEhrExtract();
-        Patient patient = ehrExtract.getPatient();
+        var ehrExtractMessageWrapper = new XmlMapper().readValue(extract, EhrExtractMessageWrapper.class);
+        var ehrExtract = ehrExtractMessageWrapper.getEhrExtract();
+        var patient = ehrExtract.getPatient();
         assertThat(patient.getNhsNumber(), equalTo("9442964410"));
+    }
+
+    @Test
+    public void shouldRetrieveSourceGpFromEHRExtract() throws JsonProcessingException {
+        var ehrExtractMessageWrapper = new XmlMapper().readValue(extract, EhrExtractMessageWrapper.class);
+        var ehrExtract = ehrExtractMessageWrapper.getEhrExtract();
+        assertThat(ehrExtract.getSourceGp(), equalTo("N82668"));
+    }
+
+    @Test
+    public void shouldRetrieveDestinationGpFromEHRExtract() throws JsonProcessingException {
+        var ehrExtractMessageWrapper = new XmlMapper().readValue(extract, EhrExtractMessageWrapper.class);
+        var ehrExtract = ehrExtractMessageWrapper.getEhrExtract();
+        assertThat(ehrExtract.getDestinationGp(), equalTo("B85002"));
     }
 }
