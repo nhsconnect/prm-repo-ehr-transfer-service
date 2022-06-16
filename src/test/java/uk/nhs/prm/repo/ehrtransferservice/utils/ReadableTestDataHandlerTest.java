7509d805f21d381bbe84c89a7e35023091d31169
Dan Moore <dan.moore1@nhs.net>
2022-06-16 12:11:43 +0100

    [PRMT-1875] Convert sanitized test messages to readable format

--- src/test/java/uk/nhs/prm/repo/ehrtransferservice/utils/ReadableTestDataHandlerTest.java
+++ src/test/java/uk/nhs/prm/repo/ehrtransferservice/utils/ReadableTestDataHandlerTest.java
@@ -2,6 +2,10 @@ package uk.nhs.prm.repo.ehrtransferservice.utils;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.w3c.dom.Document;
 
 import javax.xml.transform.OutputKeys;
@@ -10,7 +14,9 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,6 +43,30 @@ class ReadableTestDataHandlerTest {
         var messageFromReadableLoader = readableHandler.readMessage("MCCI_IN010000UK13", "FailureSanitized");
 
         assertThat(messageFromReadableLoader).isEqualTo(unreadableMessage);
+    }
+    
+    @MethodSource("testDataFilesAndNames")
+    @ParameterizedTest
+    public void shouldBeAbleToConvertAndReadMessagesBackFromReadableFormatToBeIdenticalToOriginals(String original, String interactionId, String variant) throws Exception {
+        var unreadableMessage = rawLoader.getDataAsString(original);
+
+        readableHandler.splitMessage(original);
+
+        var messageFromReadableLoader = readableHandler.readMessage(interactionId, variant);
+
+        assertThat(messageFromReadableLoader).isEqualTo(unreadableMessage);
+    }
+
+    private static Stream<Arguments> testDataFilesAndNames() {
+        return Stream.of(
+                Arguments.of("MCCI_IN010000UK13FailureSanitized", "MCCI_IN010000UK13", "FailureSanitized"),
+                Arguments.of("MCCI_IN010000UK13Empty", "MCCI_IN010000UK13", "Empty"),
+                Arguments.of("PRPA_IN000202UK01Sanitized", "PRPA_IN000202UK01", "Sanitized"),
+                Arguments.of("RCMR_IN010000UK05Sanitized", "RCMR_IN010000UK05", "Sanitized"),
+                Arguments.of("RCMR_IN030000UK06Sanitized", "RCMR_IN030000UK06", "Sanitized"),
+                Arguments.of("RCMR_IN030000UK06SanitizedWithUnexpectedBackslash", "RCMR_IN030000UK06", "SanitizedWithUnexpectedBackslash"),
+                Arguments.of("RCMR_IN030000UK06WithMidSanitized", "RCMR_IN030000UK06", "WithMidSanitized")
+        );
     }
 
     private static String prettyPrint(Document doc) {
