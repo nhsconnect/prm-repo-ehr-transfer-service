e9eb477ce06507fe4a79d2e7c46def11599b9d57
Shruthi Chandekar <shruthi.chandekar1@nhs.net>
2022-06-22 15:03:08 +0100

    [PRMT-1565] Added EnablEnableScheduling
    Co-authored-by: eyupaksu-nhs <eyup.aksu@thoughtworks.com>
    Co-authored-by: romanenhs <romane.castera1@nhs.net>

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/EhrTransferServiceApplication.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/EhrTransferServiceApplication.java
@@ -2,8 +2,10 @@ package uk.nhs.prm.repo.ehrtransferservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EhrTransferServiceApplication {
 
     public static void main(String[] args) {
