ebe227e1c092e4764cf27f1d2c9f0127bbf92e33
eyupaksu-nhs <eyup.aksu@thoughtworks.com>
2022-07-01 10:47:10 +0100

    [PRMT-2573] Introduced health-check alarm.

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -6,6 +6,10 @@ locals {
   sns_topic_error_logs_metric_name = "NumberOfNotificationsFailed"
 }
 
+data "aws_sns_topic" "alarm_notifications" {
+  name = "${var.environment}-alarm-notifications-sns-topic"
+}
+
 resource "aws_cloudwatch_log_group" "log_group" {
   name = "/nhs/deductions/${var.environment}-${data.aws_caller_identity.current.account_id}/${var.component_name}"
 
@@ -26,4 +30,21 @@ resource "aws_cloudwatch_log_metric_filter" "log_metric_filter" {
     value         = 1
     default_value = 0
   }
+}
+
+resource "aws_cloudwatch_metric_alarm" "health_metric_failure_alarm" {
+  alarm_name                = "${var.component_name}-health-metric-failure"
+  comparison_operator       = "LessThanThreshold"
+  threshold                 = "1"
+  evaluation_periods        = "3"
+  metric_name               = "Health"
+  namespace                 = local.ehr_transfer_service_metric_namespace
+  alarm_description         = "Alarm to flag failed health checks"
+  statistic                 = "Maximum"
+  treat_missing_data        = "breaching"
+  period                    = "60"
+  dimensions = {
+    "Environment" = var.environment
+  }
+  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
 }
