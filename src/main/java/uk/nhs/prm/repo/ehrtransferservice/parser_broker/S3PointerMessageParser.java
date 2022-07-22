05a81af7fed91ffaf61029991b0b6ca22d774c3c
Mario Giampietri <mario.giampietri1@nhs.net>
2022-07-22 11:20:31 +0100

    [PRMT-2712] added more details to the error log for large (type) / small (sqs sized) messages related bug

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/parser_broker/S3PointerMessageParser.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/parser_broker/S3PointerMessageParser.java
@@ -1,7 +1,5 @@
 package uk.nhs.prm.repo.ehrtransferservice.parser_broker;
 
-
-import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -14,16 +12,13 @@ public class S3PointerMessageParser {
 
     public S3PointerMessage parse(String payLoad) {
         try {
-            JsonArray messagePayload = JsonParser.parseString(payLoad).getAsJsonArray();
-            S3PointerMessage s3PointerMessage =  new S3PointerMessage(messagePayload.get(S3_POINTER_MESSAGE_CONTENT_INDEX).getAsJsonObject());
+            var messagePayload = JsonParser.parseString(payLoad).getAsJsonArray();
+            var s3PointerMessage =  new S3PointerMessage(messagePayload.get(S3_POINTER_MESSAGE_CONTENT_INDEX).getAsJsonObject());
             log.info("Successfully parsed S3PointerMessage");
             return s3PointerMessage;
         } catch (Exception e) {
-            log.error("Encountered error while parsing S3PointerMessage");
+            log.error("Encountered error while parsing S3PointerMessage", e);
             throw new IllegalArgumentException("Encountered error while parsing S3PointerMessage",e);
         }
     }
-
-
-
 }
