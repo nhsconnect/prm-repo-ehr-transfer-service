83d5bfa1acd681c8c2edea8f7956510c718ad2fb
syedsadiqali <syed.ali69@nhs.net>
2022-09-12 17:22:17 +0100

    [PRMT-2766] Log access for sqs-large-messages into new s3 bucket
    Co-authored-by: Dan Moore <dan.moore1@nhs.net>

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -11,8 +11,8 @@ resource "aws_s3_bucket" "sqs_large_message_bucket" {
   }
 
   logging {
-    target_bucket = data.aws_s3_bucket.log_bucket.id
-    target_prefix = "sqs-large-message-access-log/"
+    target_bucket = aws_s3_bucket.sqs_large_messages_s3_access_logs.id
+    target_prefix = "s3-access-logs/"
   }
 
   server_side_encryption_configuration {
@@ -60,6 +60,22 @@ resource "aws_s3_bucket_policy" "ehr-repo-sqs_large_message_bucket_policy" {
   })
 }
 
-data "aws_s3_bucket" "log_bucket" {
-  bucket = "${var.environment}-ehr-repo-log-bucket"
+resource "aws_s3_bucket" "sqs_large_messages_s3_access_logs" {
+  bucket        = "${local.sqs_large_message_bucket_name}-access-logs"
+  acl           = "private"
+  force_destroy = true
+  versioning {
+    enabled = false
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  tags = {
+    CreatedBy   = var.repo_name
+    Environment = var.environment
+  }
 }
