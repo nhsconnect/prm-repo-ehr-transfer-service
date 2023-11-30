16a45d4a6c08e87ebecb8d1cf6dc401857922e72
martin-nhs <martin.taylor22@nhs.net>
2023-11-30 10:25:36 +0000

    [PRMT-4204] Renamed poll period variables.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -42,9 +42,9 @@ variable "timeout_scheduler_fixed_rate_in_seconds" {
   default = "3600"
 }
 
-variable "processing_period_milliseconds" {
+variable "ehr_response_poll_period_milliseconds" {
   type = number
-  default = 10000 # 10 Seconds
+  default = 10000
 }
 
 variable "ehr_response_poll_limit" {
