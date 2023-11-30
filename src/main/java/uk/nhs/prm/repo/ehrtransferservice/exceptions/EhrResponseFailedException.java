e08823e27fdcc5a48af6a0490a01547b2b10d5aa
martin-nhs <martin.taylor22@nhs.net>
2023-11-30 10:50:09 +0000

    [PRMT-4204] Changed checked exceptions to unchecked.

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/EhrResponseFailedException.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/EhrResponseFailedException.java
@@ -3,7 +3,7 @@ package uk.nhs.prm.repo.ehrtransferservice.exceptions;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class EhrResponseFailedException extends Exception {
+public class EhrResponseFailedException extends RuntimeException {
     private static final String BASE_MESSAGE = "EHR Request has ended in failure, transfer status is: %s";
 
     public EhrResponseFailedException(String message) {
--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/EhrResponseTimedOutException.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/EhrResponseTimedOutException.java
@@ -3,7 +3,7 @@ package uk.nhs.prm.repo.ehrtransferservice.exceptions;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class EhrResponseTimedOutException extends Exception {
+public class EhrResponseTimedOutException extends RuntimeException {
     private static final String BASE_MESSAGE = "EHR Request has remained in a pending state and timed out in " +
             "Transfer status: %s";
 
