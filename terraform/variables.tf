45f5edd3c30591ebd6a9e6b244bd587ee8bdca9e
MohammadIqbalAD-NHS <127403145+MohammadIqbalAD-NHS@users.noreply.github.com>
2023-11-30 16:56:46 +0000

    [PRMT-4245] - Added Scale-up & Scale-down alarm in Terraform (#108)
    * [PRMT-4245] - Added Scale-up & Scale-down alarm in Terraform
    
    * [PRMT-4245] - Scale down ECS task only on pre-prod and prod

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -42,6 +42,21 @@ variable "timeout_scheduler_fixed_rate_in_seconds" {
   default = "3600"
 }
 
+variable "scale_up_expression" {
+  type    = string
+  default = "((HOUR(m1)==17 && MINUTE(m1)==58)),10,0"
+}
+
+variable "scale_down_expression" {
+  type    = string
+  default = "((HOUR(m1)==5 && MINUTE(m1)==58)),10,0"
+}
+
+variable "enable_scale_action" {
+  type    = bool
+  default = true
+}
+
 # TODO PRMT-4204 - After FoT have discussions if this is required.
 #variable "processing_period_milliseconds" {
 #  type = string
