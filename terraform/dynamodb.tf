509cdff090fbeccb0e6b812a268266c7eacee90b
martin-nhs <martin.taylor22@nhs.net>
2023-12-01 10:42:01 +0000

    [PRMT-4252-DYNAMODB] Added  `deletion_protection_enabled = true`, confirmed point_in_time_recovery is enabled.

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -2,6 +2,7 @@ resource "aws_dynamodb_table" "transfer_tracker" {
   name         = "${var.environment}-${var.component_name}-transfer-tracker"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "conversation_id"
+  deletion_protection_enabled = true
 
   server_side_encryption {
     enabled     = true
@@ -38,5 +39,4 @@ resource "aws_dynamodb_table" "transfer_tracker" {
     hash_key           = "is_active"
     projection_type    = "ALL"
   }
-
 }
