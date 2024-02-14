4161db38b11f33aa380cf7b563beabeb186772d6
martin-nhs <martin.taylor22@nhs.net>
2024-02-14 10:13:02 +0000

    [PRMT-4482] Changed each.key -> each.value

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -666,5 +666,5 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_metrics_policy_attach" {
 module "sns_enforce_https" {
   source        = "modules/sns_enforce_https"
   for_each      = toset(local.sns_topic_arns)
-  sns_topic_arn = each.key
+  sns_topic_arn = each.value
 }
