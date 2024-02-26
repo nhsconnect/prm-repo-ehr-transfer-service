f2ff0adb966f4ff21eb2e2ce958f39d6f6064e65
martin-nhs <martin.taylor22@nhs.net>
2024-02-26 15:30:22 +0000

    [PRMT-4524] Added TransferTableAttribute.java

---
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/database/TransferTableAttribute.java
@@ -0,0 +1,26 @@
+package uk.nhs.prm.repo.ehrtransferservice.database;
+
+/**
+ * An enumeration which represents all possible
+ * attributes within the Transfer Tracker DynamoDB
+ * table.
+ */
+public enum TransferTableAttribute {
+    INBOUND_CONVERSATION_ID("InboundConversationId"),
+    OUTBOUND_CONVERSATION_ID("OutboundConversationId"),
+    NHS_NUMBER("NhsNumber"),
+    SOURCE_GP("SourceGp"),
+    DESTINATION_GP("DestinationGp"),
+    STATE("State"),
+    MESH_MESSAGE_ID("MeshMessageId"),
+    NEMS_MESSAGE_ID("NemsMessageId"),
+    CREATED_AT("CreatedAt"),
+    UPDATED_UP("UpdatedAt"),
+    DELETED_AT("DeletedAt");
+
+    public final String name;
+
+    TransferTableAttribute(String name) {
+        this.name = name;
+    }
+}
