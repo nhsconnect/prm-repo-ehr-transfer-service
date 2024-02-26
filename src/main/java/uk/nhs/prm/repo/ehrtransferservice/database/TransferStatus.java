134c9668339f1410d0e64fecae513f182244f3af
martin-nhs <martin.taylor22@nhs.net>
2024-02-26 13:22:59 +0000

    [PRMT-4524] Created initial enum to hold status values.

---
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/database/TransferStatus.java
@@ -0,0 +1,16 @@
+package uk.nhs.prm.repo.ehrtransferservice.database;
+
+public enum TransferStatus {
+    EHR_REQUEST_SENT(1),
+    LARGE_EHR_CONTINUE_REQUEST_SENT(2),
+    TRANSFER_TO_REPO_STARTED(3),
+    EHR_TRANSFER_TO_REPO_COMPLETE(4),
+    EHR_TRANSFER_FAILED(5),
+    EHR_TRANSFER_TIMEOUT(6);
+
+    public final int code;
+
+    TransferStatus(int code) {
+        this.code = code;
+    }
+}
