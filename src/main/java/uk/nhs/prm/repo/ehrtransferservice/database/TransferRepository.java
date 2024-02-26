7bf34cfad94262c92083399d5fbb35f67d6cf99a
martin-nhs <martin.taylor22@nhs.net>
2024-02-26 13:28:57 +0000

    [PRMT-4524] Injected DynamoDbClient and AppConfig into TransferRepository.java

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/database/TransferRepository.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/database/TransferRepository.java
@@ -1,13 +1,20 @@
 package uk.nhs.prm.repo.ehrtransferservice.database;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import uk.nhs.prm.repo.ehrtransferservice.config.AppConfig;
 import uk.nhs.prm.repo.ehrtransferservice.database.model.ConversationRecord;
 
 import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class TransferRepository {
-    public void createConversation() {
+    private final DynamoDbClient client;
+    private final AppConfig config;
+
+    public void createConversation(ConversationRecord record) {
         throw new UnsupportedOperationException();
     }
 
@@ -23,7 +30,7 @@ public class TransferRepository {
         throw new UnsupportedOperationException();
     }
 
-    public void updateConversationStatus(UUID inboundConversationId, String status) {
+    public void updateConversationStatus(UUID inboundConversationId, TransferStatus status) {
         throw new UnsupportedOperationException();
     }
 }
