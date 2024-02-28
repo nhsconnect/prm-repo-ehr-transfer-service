29701da96c05ee8a65149987c5cf8f62b52be77f
martin-nhs <martin.taylor22@nhs.net>
2024-02-28 10:04:00 +0000

    [PRMT-4524] Finished implementation of ValidationUtility.java

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/validator/ValidationUtility.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/validator/ValidationUtility.java
@@ -5,14 +5,13 @@ import uk.nhs.prm.repo.ehrtransferservice.database.TransferStatus;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public final class ValidationUtility {
     private ValidationUtility() { }
 
-    private static final Pattern NHS_NUMBER_REGEX = Pattern.compile("\\b\\d{3}\\d{3}\\d{4}\\b");
+    private static final String NHS_NUMBER_REGEX = "\\b\\d{3}\\d{3}\\d{4}\\b";
 
-    private static final Pattern ODS_CODE_REGEX = Pattern.compile("(U\\d{5}|[AC]\\d{3}|V\\d{4}|[A-Za-z0-9]{5}\\d{3})$");
+    private static final String ODS_CODE_REGEX = "(U\\d{5}|[AC]\\d{3}|V\\d{4}|[A-Za-z0-9]{5}\\d{3})$";
 
     private static final List<String> STATUSES;
 
@@ -25,27 +24,25 @@ public final class ValidationUtility {
     /**
      * Checks if a valid NHS number is present.
      * @param nhsNumber The NHS number to be validated.
-     * @return true is a valid NHS number is present, otherwise false.
+     * @return true if a valid NHS number is present, otherwise false.
      */
     public static boolean isValidNhsNumber(String nhsNumber) {
-        return false;
+        return nhsNumber.matches(NHS_NUMBER_REGEX);
     }
 
     /**
-     * Primary Care Networks (PCN): PCN ODS codes are 6-character codes, all starting with a ‘U’ followed by 5 numbers, for example, U12345.
-     * Care Home and Domiciliary Care providers: These providers have at least 2 codes. An HQ code or “parent” code is 4 digits and normally starts with an A or C, e.g. A*** or C***. One or more site or “child” codes are 5 digits and normally start with V, e.g. V****.
-     * Branch Surgery codes: These are allocated using the existing NHS PS GP Practice code for the parent Surgery, to which a further three numerical characters are added to define each branch e.g. 001, 002 etc.
-     * @param odsCode The ODS code needing to be validated.
+     * Checks if a valid ODS code is present.
+     * @param odsCode The ODS code to be validated.
      * @return true if the ODS code is valid, otherwise false.
      */
     public static boolean isValidOdsCode(String odsCode) {
-        return false;
+        return odsCode.matches(ODS_CODE_REGEX);
     }
 
     /**
      * Checks if a valid status is present.
      * @param status The status to be validated.
-     * @return true is the status is valid, otherwise false.
+     * @return true if the status is valid, otherwise false.
      */
     public static boolean isValidStatus(String status) {
         return STATUSES.contains(status);
