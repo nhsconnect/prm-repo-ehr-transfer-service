03d5821a29036d4b8ef7655042a172d4774acea1
martin-nhs <martin.taylor22@nhs.net>
2024-02-29 11:29:36 +0000

    [PRMT-4524] Added existing states to TransferState.java

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/database/TransferState.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/database/TransferState.java
@@ -1,7 +1,11 @@
 package uk.nhs.prm.repo.ehrtransferservice.database;
 
 public enum TransferState {
-    EHR_TRANSFER_STARTED(1);
+    EHR_TRANSFER_STARTED(1),
+    EHR_REQUEST_SENT_TO_GP2GP_MESSENGER(2),
+    EHR_TRANSFER_TO_REPO_COMPLETE(3),
+    EHR_TRANSFER_FAILED(4),
+    EHR_TRANSFER_TIMEOUT(5);
 
     public final int code;
 
