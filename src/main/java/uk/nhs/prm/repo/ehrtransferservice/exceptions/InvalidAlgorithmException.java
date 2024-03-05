e8d9a098709687ab6438cb569b39c57bd1830e86
martin-nhs <martin.taylor22@nhs.net>
2024-03-05 15:48:31 +0000

    [PRMT-4580] Removed unused exception

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/InvalidAlgorithmException.java
+++
@@ -1,8 +0,0 @@
-package uk.nhs.prm.repo.ehrtransferservice.exceptions;
-
-public class InvalidAlgorithmException extends RuntimeException {
-    private static final String EXCEPTION_MESSAGE = "An invalid algorithm was provided, details: %s.";
-    public InvalidAlgorithmException(String details) {
-        super(EXCEPTION_MESSAGE.formatted(details));
-    }
-}
