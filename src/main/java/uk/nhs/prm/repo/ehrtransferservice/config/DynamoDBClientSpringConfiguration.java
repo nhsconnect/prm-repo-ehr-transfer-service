6a1ab9d722fc4650ccce766b108bfa6090369816
martin-nhs <martin.taylor22@nhs.net>
2024-03-05 13:31:17 +0000

    [PRMT-4524] Removed Dynamo Enhanced client due to time constraints, will be added later.

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/config/DynamoDBClientSpringConfiguration.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/config/DynamoDBClientSpringConfiguration.java
@@ -5,12 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import uk.nhs.prm.repo.ehrtransferservice.database.model.TransferTracker;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,26 +17,10 @@ public class DynamoDBClientSpringConfiguration {
     private AppConfig appConfig;
 
     @Bean
-    @Deprecated // To be removed in favour of using directly through the enhanced client below.
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
                 .region(Region.of(awsRegion))
                 .credentialsProvider(awsCredentialsProvider)
                 .build();
-    }
-
-    @Bean
-    public DynamoDbEnhancedClient dynamoDbEnhancedClient() {
-        return DynamoDbEnhancedClient.builder()
-                .dynamoDbClient(dynamoDbClient())
-                .build();
-    }
-
-    @Bean
-    public DynamoDbTable<TransferTracker> transferTrackerDynamoDbTable() {
-        return dynamoDbEnhancedClient().table(
-                appConfig.transferTrackerDbTableName(),
-                TableSchema.fromBean(TransferTracker.class)
-        );
     }
 }
