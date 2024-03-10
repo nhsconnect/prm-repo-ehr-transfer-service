18ff311a4d578948b5bde8c4ac4556579e09e828
martin-nhs <martin.taylor22@nhs.net>
2024-03-10 18:44:29 +0000

    [PRMT-4524] Added exceptions

---
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/EhrCoreMessageIdNotPresentException.java
@@ -0,0 +1,5 @@
+package uk.nhs.prm.repo.ehrtransferservice.exceptions;
+
+public class EhrCoreMessageIdNotPresentException extends RuntimeException {
+    public EhrCoreMessageIdNotPresentException() { }
+}
