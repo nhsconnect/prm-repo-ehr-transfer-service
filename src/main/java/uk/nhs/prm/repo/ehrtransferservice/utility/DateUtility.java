ed0c35006e77a31cdd71231ecff82537d53dc596
martin-nhs <martin.taylor22@nhs.net>
2024-03-12 22:19:16 +0000

    [PRMT-4524] Added DateUtility.java

---
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/utility/DateUtility.java
@@ -0,0 +1,21 @@
+package uk.nhs.prm.repo.ehrtransferservice.utility;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public final class DateUtility {
+    private DateUtility() { }
+    public static final String ZONE_ID = "Europe/London";
+
+    /**
+     * Gets an ISO timestamp in the format we require for DynamoDB.
+     * @return The formatted String timestamp.
+     */
+    public static String getIsoTimestamp() {
+        return ZonedDateTime.now(ZoneId.of(ZONE_ID))
+            .truncatedTo(ChronoUnit.MINUTES)
+            .format(DateTimeFormatter.ISO_DATE_TIME);
+    }
+}
