e0675823adb4488405651e9d2ea18622e813e08a
martin-nhs <martin.taylor22@nhs.net>
2024-03-13 17:29:00 +0000

    [PRMT-4524] Refactored logic in Gp2gpMessengerService.java

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/handlers/LargeMessageFragmentHandler.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/handlers/LargeMessageFragmentHandler.java
@@ -3,19 +3,17 @@ package uk.nhs.prm.repo.ehrtransferservice.handlers;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import uk.nhs.prm.repo.ehrtransferservice.database.TransferService;
 import uk.nhs.prm.repo.ehrtransferservice.gp2gp_message_models.ParsedMessage;
 import uk.nhs.prm.repo.ehrtransferservice.models.LargeEhrFragmentMessage;
 import uk.nhs.prm.repo.ehrtransferservice.services.ehr_repo.EhrRepoService;
 import uk.nhs.prm.repo.ehrtransferservice.services.ehr_repo.StoreMessageResult;
+import uk.nhs.prm.repo.ehrtransferservice.services.gp2gp_messenger.Gp2gpMessengerService;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class LargeMessageFragmentHandler implements MessageHandler<ParsedMessage> {
-
-    private final TransferService transferService;
-
+    private final Gp2gpMessengerService gp2gpMessengerService;
     private final EhrRepoService ehrRepoService;
 
     @Override
@@ -24,7 +22,7 @@ public class LargeMessageFragmentHandler implements MessageHandler<ParsedMessage
 
         if (storeMessageResult.isEhrComplete()) {
             log.info("Successfully stored all fragments of large ehr message in the ehr-repo-service");
-            transferService.markConversationAsComplete(fragmentMessage.getConversationId());
+            gp2gpMessengerService.sendEhrCompletePositiveAcknowledgement(fragmentMessage.getConversationId());
         }
     }
 
