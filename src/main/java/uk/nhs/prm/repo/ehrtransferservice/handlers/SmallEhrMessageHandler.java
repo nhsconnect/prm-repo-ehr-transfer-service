4d85d8284196ce4e2432a88478a1643b38b7435a
martin-nhs <martin.taylor22@nhs.net>
2024-03-13 17:29:49 +0000

    [PRMT-4524] Refactored logic in Gp2gpMessengerService.java

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/handlers/SmallEhrMessageHandler.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/handlers/SmallEhrMessageHandler.java
@@ -3,9 +3,10 @@ package uk.nhs.prm.repo.ehrtransferservice.handlers;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import uk.nhs.prm.repo.ehrtransferservice.database.TransferService;
 import uk.nhs.prm.repo.ehrtransferservice.gp2gp_message_models.ParsedMessage;
 import uk.nhs.prm.repo.ehrtransferservice.services.ehr_repo.EhrRepoService;
+import uk.nhs.prm.repo.ehrtransferservice.services.ehr_repo.StoreMessageResult;
+import uk.nhs.prm.repo.ehrtransferservice.services.gp2gp_messenger.Gp2gpMessengerService;
 
 @Slf4j
 @Service
@@ -14,14 +15,16 @@ public class SmallEhrMessageHandler implements MessageHandler<ParsedMessage> {
 
     private final EhrRepoService ehrRepoService;
 
-    private final TransferService transferService;
+    private final Gp2gpMessengerService gp2gpMessengerService;
 
     @Override
     public void handleMessage(ParsedMessage parsedMessage) throws Exception {
-        ehrRepoService.storeMessage(parsedMessage);
-        log.info("The Small EHR has been stored in the ehr-repo-service successfully");
-        // TODO PRMT-4524 make a call to transferService.markConversationAsComplete
-        // TODO PRMT-4524 make a call to audit as we do in the LargeEhrCoreMessageHandler???
+        final StoreMessageResult storeMessageResult = ehrRepoService.storeMessage(parsedMessage);
+        log.info("The Small EHR with Inbound Message ID %s has been stored in the ehr-repo-service successfully"
+            .formatted(parsedMessage.getConversationId()));
 
+        if(storeMessageResult.isEhrComplete()) {
+            gp2gpMessengerService.sendEhrCompletePositiveAcknowledgement(parsedMessage.getConversationId());
+        }
     }
 }
