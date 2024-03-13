27fec1496ea5b1d938310ad64fcef221cb8d06f4
martin-nhs <martin.taylor22@nhs.net>
2024-03-13 17:25:05 +0000

    [PRMT-4524] Added exception

---
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/UpdateConversationException.java
@@ -0,0 +1,14 @@
+package uk.nhs.prm.repo.ehrtransferservice.exceptions;
+
+import uk.nhs.prm.repo.ehrtransferservice.exceptions.base.DatabaseException;
+
+import java.util.UUID;
+
+public class UpdateConversationException extends DatabaseException {
+    private static final String EXCEPTION_MESSAGE =
+        "The conversation could not be updated with Inbound Message ID %s";
+
+    public UpdateConversationException(UUID inboundConversationId) {
+        super(EXCEPTION_MESSAGE.formatted(inboundConversationId));
+    }
+}
