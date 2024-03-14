e42e12231e8594be588352d194a8e8d6e4e357ab
martin-nhs <martin.taylor22@nhs.net>
2024-03-14 22:04:24 +0000

    [PRMT-4524] YAGI statuses.
    Signed-off-by: martin-nhs <martin.taylor22@nhs.net>

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/database/enumeration/CoreTransferStatus.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/database/enumeration/CoreTransferStatus.java
@@ -1,8 +1,5 @@
 package uk.nhs.prm.repo.ehrtransferservice.database.enumeration;
 
 public enum CoreTransferStatus {
-    INBOUND_COMPLETE,
-    OUTBOUND_SENT,
-    OUTBOUND_COMPLETE,
-    OUTBOUND_FAILED
+    OUTBOUND_SENT
 }
