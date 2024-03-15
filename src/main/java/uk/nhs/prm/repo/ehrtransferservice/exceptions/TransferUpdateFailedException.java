2d7818411e9664a6298195fac7b710aaf55b4aff
martin-nhs <martin.taylor22@nhs.net>
2024-03-15 11:06:58 +0000

    [PRMT-4524] Renamed exception.
    Signed-off-by: martin-nhs <martin.taylor22@nhs.net>

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/TransferUpdateFailedException.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/TransferUpdateFailedException.java
@@ -4,11 +4,11 @@ import uk.nhs.prm.repo.ehrtransferservice.exceptions.base.DatabaseException;
 
 import java.util.UUID;
 
-public class TransferUnableToUpdateException extends DatabaseException {
+public class TransferUpdateFailedException extends DatabaseException {
     private static final String EXCEPTION_MESSAGE =
         "Unable to update transfer record for Inbound Conversation ID %s.";
 
-    public TransferUnableToUpdateException(UUID inboundConversationId, Throwable throwable) {
+    public TransferUpdateFailedException(UUID inboundConversationId, Throwable throwable) {
         super(EXCEPTION_MESSAGE.formatted(inboundConversationId.toString()), throwable);
     }
 }
