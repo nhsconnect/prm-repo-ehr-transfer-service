7cce6fb1a5d18979caade99319292f8b2c28b10f
martin-nhs <martin.taylor22@nhs.net>
2024-03-15 11:07:51 +0000

    [PRMT-4524] Removed AWS exception handling.
    Signed-off-by: martin-nhs <martin.taylor22@nhs.net>

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/database/TransferService.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/database/TransferService.java
@@ -1,13 +1,10 @@
 package uk.nhs.prm.repo.ehrtransferservice.database;
 
-import com.amazonaws.SdkClientException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import uk.nhs.prm.repo.ehrtransferservice.database.enumeration.ConversationTransferStatus;
 import uk.nhs.prm.repo.ehrtransferservice.database.model.ConversationRecord;
-import uk.nhs.prm.repo.ehrtransferservice.exceptions.FailedToPersistException;
-import uk.nhs.prm.repo.ehrtransferservice.exceptions.TransferUnableToUpdateException;
 import uk.nhs.prm.repo.ehrtransferservice.repo_incoming.RepoIncomingEvent;
 
 import java.util.Optional;
@@ -22,18 +19,16 @@ public class TransferService {
     private final TransferRepository transferRepository;
 
     public void createConversation(RepoIncomingEvent event) {
-        final UUID inboundConversationId = UUID.fromString(event.getConversationId());
-
-        try {
-            transferRepository.createConversation(event);
-            log.info("Initial Conversation created for Inbound Conversation ID {}.", event.getConversationId());
-        } catch (SdkClientException exception) {
-            throw new FailedToPersistException(inboundConversationId, exception);
-        }
+        transferRepository.createConversation(event);
+        log.info("Initial conversation record created for Inbound Conversation ID {}", event.getConversationId());
     }
 
     public ConversationRecord getConversationByInboundConversationId(UUID inboundConversationId) {
-        return transferRepository.findConversationByInboundConversationId(inboundConversationId);
+        final ConversationRecord conversationRecord =
+            transferRepository.findConversationByInboundConversationId(inboundConversationId);
+
+        log.info("Found conversation record for Inbound Conversation ID {}", inboundConversationId);
+        return conversationRecord;
     }
 
     public Optional<UUID> getNemsMessageIdAsUuid(UUID inboundConversationId) {
@@ -51,31 +46,31 @@ public class TransferService {
     }
 
     public boolean isInboundConversationIdPresent(UUID inboundConversationId) {
-        return transferRepository.isInboundConversationPresent(inboundConversationId);
+        final boolean conversationPresent = transferRepository
+            .isInboundConversationPresent(inboundConversationId);
+
+        if (conversationPresent) {
+            log.info("Conversation record found for Inbound Conversation ID {}", inboundConversationId);
+        } else {
+            log.info("Conversation record not found for Inbound Conversation ID {}", inboundConversationId);
+        }
+
+        return conversationPresent;
     }
 
     public void updateConversationTransferStatus(UUID inboundConversationId, ConversationTransferStatus conversationTransferStatus) {
-        try {
-            transferRepository.updateConversationStatus(inboundConversationId, conversationTransferStatus);
-            log.info("Updated conversationTransferStatus of EHR transfer in DB to: {}.", conversationTransferStatus);
-        } catch (SdkClientException exception) {
-            log.error("Failed to update conversationTransferStatus of EHR Transfer: " + exception.getMessage());
-            throw new TransferUnableToUpdateException(inboundConversationId, exception);
-        }
+        transferRepository.updateConversationStatus(inboundConversationId, conversationTransferStatus);
+        log.info("Updated conversation record with Inbound Conversation ID {} with the status of {}",
+            inboundConversationId, conversationTransferStatus.name());
     }
 
     public void updateConversationTransferStatusWithFailure(UUID inboundConversationId, String failureCode) {
-        try {
-            transferRepository.updateConversationStatusWithFailure(inboundConversationId, failureCode);
-            log.info("Updated conversationTransferStatus of EHR transfer in DB to: {}", INBOUND_FAILED);
-        } catch (SdkClientException exception) {
-            log.error("Failed to update conversationTransferStatus of EHR Transfer: " + exception.getMessage());
-            throw new TransferUnableToUpdateException(inboundConversationId, exception);
-        }
+        transferRepository.updateConversationStatusWithFailure(inboundConversationId, failureCode);
+        log.info("Updated conversation record with Inbound Conversation ID {} to {}, with failure code {}",
+            inboundConversationId, INBOUND_FAILED.name(), failureCode);
     }
 
     public UUID getEhrCoreInboundMessageIdForInboundConversationId(UUID inboundConversationId) {
-        return transferRepository
-            .getEhrCoreInboundMessageIdForInboundConversationId(inboundConversationId);
+        return transferRepository.getEhrCoreInboundMessageIdForInboundConversationId(inboundConversationId);
     }
 }
