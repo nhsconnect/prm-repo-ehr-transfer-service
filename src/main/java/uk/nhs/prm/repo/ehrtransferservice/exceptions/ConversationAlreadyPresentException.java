54b7e8b97135ca3eac48371a01e47cc528244de6
martin-nhs <martin.taylor22@nhs.net>
2024-03-18 10:17:59 +0000

    [PRMT-4524] Fixed typo.
    Signed-off-by: martin-nhs <martin.taylor22@nhs.net>

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/ConversationAlreadyPresentException.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/ConversationAlreadyPresentException.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 
 public class ConversationAlreadyPresentException extends DatabaseException {
     private static final String EXCEPTION_MESSAGE =
-        "The conversation already exists for Inbound Message ID %s";
+        "The conversation already exists for Inbound Conversation ID %s";
 
     public ConversationAlreadyPresentException(UUID inboundConversationId) {
         super(EXCEPTION_MESSAGE.formatted(inboundConversationId));
