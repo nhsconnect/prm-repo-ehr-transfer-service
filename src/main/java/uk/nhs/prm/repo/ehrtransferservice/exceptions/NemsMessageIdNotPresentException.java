664cbbe6d5b633d26dfa2b0108ad8a33113bc6b8
martin-nhs <martin.taylor22@nhs.net>
2024-03-18 10:57:37 +0000

    [PRMT-4524] Removed unused exception
    Signed-off-by: martin-nhs <martin.taylor22@nhs.net>

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/NemsMessageIdNotPresentException.java
+++
@@ -1,5 +0,0 @@
-package uk.nhs.prm.repo.ehrtransferservice.exceptions;
-
-public class NemsMessageIdNotPresentException extends RuntimeException {
-    public NemsMessageIdNotPresentException() { }
-}
