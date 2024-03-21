e071adbb7557c39d039c0429f18cbcd9fd54946f
martin-nhs <martin.taylor22@nhs.net>
2024-03-21 12:51:41 +0000

    [PRMT-4407] Added required terraform.
    Signed-off-by: martin-nhs <martin.taylor22@nhs.net>

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -650,3 +650,27 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_metrics_policy_attach" {
   role       = aws_iam_role.component-ecs-role.name
   policy_arn = aws_iam_policy.cloudwatch_metrics_policy.arn
 }
+
+data "aws_iam_policy_document" "ehr_transfer_tracker_db_access" {
+  statement {
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:Query"
+    ]
+    resources = [
+      "arn:aws:dynamodb:${var.region}:${data.aws_caller_identity.current.account_id}:table/${var.environment}-ehr-transfer-tracker"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ehr_transfer_tracker_db_access" {
+  name   = "${var.environment}-${var.component_name}-transfer-tracker-db-access"
+  policy = data.aws_iam_policy_document.ehr_transfer_tracker_db_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_application_user_policy_attach" {
+  role       = "${var.environment}-${var.component_name}-EcsTaskRole"
+  policy_arn = aws_iam_policy.ehr_transfer_tracker_db_access.arn
+}
