55b94ff548bdf4351af0b43c095fe6f550caf36d
chrisbloe-nhse <151756494+chrisbloe-nhse@users.noreply.github.com>
2024-04-02 12:48:56 +0100

    Remove DynamoDB Scan capability for ecs task (#249)
    Co-authored-by: Kris Bloe <kris.bloe@answerdigital.com>

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -186,8 +186,7 @@ resource "aws_iam_role_policy_attachment" "ecs_dynamo_attach" {
 data "aws_iam_policy_document" "transfer-tracker-db-indexes-access" {
   statement {
     actions = [
-      "dynamodb:Query",
-      "dynamodb:Scan"
+      "dynamodb:Query"
     ]
     resources = [
       "arn:aws:dynamodb:${var.region}:${data.aws_caller_identity.current.account_id}:table/${aws_dynamodb_table.transfer_tracker.name}/index/*"
