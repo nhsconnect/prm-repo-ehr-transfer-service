dcbf0a59e74952259f3114209dbc225444626e9e
martin-nhs <martin.taylor22@nhs.net>
2024-05-03 11:25:00 +0100

    [PRMT-4817] Removed old timeout logic.
    Signed-off-by: martin-nhs <martin.taylor22@nhs.net>

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/repo_incoming/RepoIncomingService.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/repo_incoming/RepoIncomingService.java
@@ -5,15 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.nhs.prm.repo.ehrtransferservice.database.TransferService;
-import uk.nhs.prm.repo.ehrtransferservice.exceptions.EhrResponseFailedException;
-import uk.nhs.prm.repo.ehrtransferservice.exceptions.EhrResponseTimedOutException;
 import uk.nhs.prm.repo.ehrtransferservice.services.AuditService;
+import uk.nhs.prm.repo.ehrtransferservice.services.TimeoutService;
 import uk.nhs.prm.repo.ehrtransferservice.services.gp2gp_messenger.Gp2gpMessengerService;
 
 import java.util.Optional;
 import java.util.UUID;
 
-import static uk.nhs.prm.repo.ehrtransferservice.database.enumeration.ConversationTransferStatus.*;
+import static uk.nhs.prm.repo.ehrtransferservice.database.enumeration.ConversationTransferStatus.INBOUND_REQUEST_SENT;
 
 @Slf4j
 @Service
@@ -22,6 +21,7 @@ public class RepoIncomingService {
     private final AuditService auditService;
     private final TransferService transferService;
     private final Gp2gpMessengerService gp2gpMessengerService;
+    private final TimeoutService timeoutService;
 
     @Value("${ehrResponsePollLimit}")
     private int ehrResponsePollLimit;
@@ -41,38 +41,6 @@ public class RepoIncomingService {
         transferService.updateConversationTransferStatus(inboundConversationId, INBOUND_REQUEST_SENT);
 
         auditService.publishAuditMessage(inboundConversationId, INBOUND_REQUEST_SENT, nemsMessageId);
-        waitForTransferTrackerDbToUpdate(inboundConversationId);
-    }
-
-    private void waitForTransferTrackerDbToUpdate(UUID inboundConversationId)
-        throws InterruptedException, EhrResponseFailedException, EhrResponseTimedOutException {
-        int pollCount = 0;
-        String transferState;
-
-        log.info("Awaiting for transfer status to become INBOUND_COMPLETE for Inbound Conversation ID {}",
-            inboundConversationId.toString().toUpperCase());
-
-        do {
-            Thread.sleep(ehrResponsePollPeriodMilliseconds);
-            transferState = transferService.getConversationTransferStatus(inboundConversationId);
-
-            log.info("Transfer status yet to be updated to INBOUND_COMPLETE (currently {}) for Inbound Conversation ID {} - attempt {} of {}",
-                transferState,
-                inboundConversationId.toString().toUpperCase(),
-                pollCount + 1,
-                ehrResponsePollLimit
-            );
-
-            if (transferState.equals(INBOUND_COMPLETE.name())) {
-                log.info("Transfer status has been set to INBOUND_COMPLETE for Inbound Conversation ID {}", inboundConversationId.toString().toUpperCase());
-                return;
-            }
-
-            pollCount++;
-        } while (pollCount < ehrResponsePollLimit);
-
-        log.warn("The transfer for Inbound Conversation ID {} has timed out", inboundConversationId.toString().toUpperCase());
-        transferService.updateConversationTransferStatus(inboundConversationId, INBOUND_TIMEOUT);
-        throw new EhrResponseTimedOutException(transferState);
+        timeoutService.add(inboundConversationId);
     }
 }
