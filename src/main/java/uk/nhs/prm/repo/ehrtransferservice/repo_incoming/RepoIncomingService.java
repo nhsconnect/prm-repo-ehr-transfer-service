9e9a0fbdf943b7017b5616eff6b1d38ba7805466
martin-nhs <martin.taylor22@nhs.net>
2024-05-07 13:53:33 +0100

    [PRMT-4817] Added new timeout logic to RepoIncomingService.java
    Signed-off-by: martin-nhs <martin.taylor22@nhs.net>

--- src/main/java/uk/nhs/prm/repo/ehrtransferservice/repo_incoming/RepoIncomingService.java
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/repo_incoming/RepoIncomingService.java
@@ -1,33 +1,45 @@
 package uk.nhs.prm.repo.ehrtransferservice.repo_incoming;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.nhs.prm.repo.ehrtransferservice.database.TransferService;
+import uk.nhs.prm.repo.ehrtransferservice.exceptions.timeout.TimeoutExceededException;
 import uk.nhs.prm.repo.ehrtransferservice.services.AuditService;
-import uk.nhs.prm.repo.ehrtransferservice.services.TimeoutService;
 import uk.nhs.prm.repo.ehrtransferservice.services.gp2gp_messenger.Gp2gpMessengerService;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import static uk.nhs.prm.repo.ehrtransferservice.database.enumeration.ConversationTransferStatus.INBOUND_REQUEST_SENT;
+import static uk.nhs.prm.repo.ehrtransferservice.database.enumeration.ConversationTransferStatus.INBOUND_TIMEOUT;
+import static uk.nhs.prm.repo.ehrtransferservice.utility.InboundTimeoutTracker.*;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class RepoIncomingService {
     private final AuditService auditService;
     private final TransferService transferService;
     private final Gp2gpMessengerService gp2gpMessengerService;
-    private final TimeoutService timeoutService;
 
-    @Value("${ehrResponsePollLimit}")
-    private int ehrResponsePollLimit;
+    private final int pollPeriodMilliseconds;
+    private final int inboundTimeoutMinutes;
 
-    @Value("${ehrResponsePollPeriodMilliseconds}")
-    private int ehrResponsePollPeriodMilliseconds;
+    @Autowired
+    public RepoIncomingService(
+        AuditService auditService,
+        TransferService transferService,
+        Gp2gpMessengerService gp2gpMessengerService,
+        @Value("${ehrResponsePollPeriodMilliseconds}") int pollPeriodMilliseconds,
+        @Value("${inboundTimeoutMinutes}") int inboundTimeoutMinutes
+    ) {
+        this.auditService = auditService;
+        this.transferService = transferService;
+        this.gp2gpMessengerService = gp2gpMessengerService;
+        this.pollPeriodMilliseconds = pollPeriodMilliseconds;
+        this.inboundTimeoutMinutes = inboundTimeoutMinutes;
+    }
 
     public void processIncomingEvent(RepoIncomingEvent repoIncomingEvent) throws Exception {
         final UUID inboundConversationId = UUID.fromString(repoIncomingEvent.getConversationId());
@@ -35,12 +47,31 @@ public class RepoIncomingService {
             UUID.fromString(repoIncomingEvent.getNemsMessageId())
         );
 
+        captureConversationActivityTimestamp(inboundConversationId);
+
         transferService.createConversation(repoIncomingEvent);
 
         gp2gpMessengerService.sendEhrRequest(repoIncomingEvent);
         transferService.updateConversationTransferStatus(inboundConversationId, INBOUND_REQUEST_SENT);
 
         auditService.publishAuditMessage(inboundConversationId, INBOUND_REQUEST_SENT, nemsMessageId);
-        timeoutService.waitForRecordReceived(inboundConversationId);
+        waitForConversationToComplete(inboundConversationId);
+    }
+
+    private void waitForConversationToComplete(UUID inboundConversationId) throws InterruptedException  {
+        log.info("Awaiting Inbound Conversation ID {} to complete",
+            inboundConversationId.toString().toUpperCase());
+
+        while (isConversationActive(inboundConversationId)) {
+            Thread.sleep(pollPeriodMilliseconds);
+            verifyConversationNotTimedOut(inboundConversationId);
+        }
+    }
+
+    private void verifyConversationNotTimedOut(UUID inboundConversationId) {
+        if (isConversationTimedOut(inboundConversationId, inboundTimeoutMinutes)) {
+            transferService.updateConversationTransferStatus(inboundConversationId, INBOUND_TIMEOUT);
+            throw new TimeoutExceededException(inboundConversationId);
+        }
     }
 }
