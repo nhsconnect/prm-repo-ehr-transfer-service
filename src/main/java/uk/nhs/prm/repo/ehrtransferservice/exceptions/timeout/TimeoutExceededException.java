293312029a9c7769b323b7e9b93c8cda3217513e
martin-nhs <martin.taylor22@nhs.net>
2024-05-07 11:44:58 +0100

    [PRMT-4817] Added TimeoutExceededException.java
    Signed-off-by: martin-nhs <martin.taylor22@nhs.net>

---
+++ src/main/java/uk/nhs/prm/repo/ehrtransferservice/exceptions/timeout/TimeoutExceededException.java
@@ -0,0 +1,13 @@
+package uk.nhs.prm.repo.ehrtransferservice.exceptions.timeout;
+
+import uk.nhs.prm.repo.ehrtransferservice.exceptions.base.TimeoutException;
+
+import java.util.UUID;
+
+public class TimeoutExceededException extends TimeoutException {
+    private static final String EXCEPTION_MESSAGE = "The request for Inbound Conversation ID %s has timed out";
+
+    public TimeoutExceededException(UUID inboundConversationId) {
+        super(EXCEPTION_MESSAGE.formatted(inboundConversationId));
+    }
+}
