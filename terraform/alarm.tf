919d081f55504072237ea918bd8b696798e294db
Andy Flint <andrew.flint@answerdigital.com>
2024-10-28 15:43:21 +0000

    [PRMP-1076] Removed perf environment from scale up & down alarms

--- terraform/alarm.tf
+++ terraform/alarm.tf
@@ -343,7 +343,7 @@ resource "aws_cloudwatch_metric_alarm" "splunk_uploader_sns_topic_error_log_alar
 }
 
 resource "aws_cloudwatch_metric_alarm" "scale_up" {
-  count = contains(["dev", "test", "perf"], var.environment) ? 1 : 0
+  count = contains(["dev", "test"], var.environment) ? 1 : 0
 
   alarm_name          = "${var.environment}-${var.component_name}-scale-up"
   comparison_operator = "GreaterThanOrEqualToThreshold"
@@ -378,7 +378,7 @@ resource "aws_cloudwatch_metric_alarm" "scale_up" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "scale_down" {
-  count = contains(["dev", "test", "perf"], var.environment) ? 1 : 0
+  count = contains(["dev", "test"], var.environment) ? 1 : 0
 
   alarm_name          = "${var.environment}-${var.component_name}-scale-down"
   comparison_operator = "GreaterThanOrEqualToThreshold"
